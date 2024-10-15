cfc2a8ac9e215fee3852e35c282c02d789c8ab07
Ur Koren <ork14790@gmail.com>
2024-10-15 10:47:22 +0300

    Split the repo

--- app/config.py
+++
@@ -1,4 +0,0 @@
-"""
-Module to be used for configs
-"""
-KEY = "key=ETK6N5APZBV3DZNEDLK53S5JY"
--- app/wsgi.py
+++
@@ -1,8 +0,0 @@
-"""
-The main module that runs the application
-"""
-
-from flaskapp import app
-
-if __name__ == "__main__":
-    app.run()
